f6306ba2a7bf9cd9ef239c70869d756e0081ea82
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-01 11:10:23 +0300

    nt cng

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -1,7 +1,7 @@
 const config = {
   user: 'MertAdmin',
   password: 'Admin12345',
-  server: 'https://cryptic-harbor-96739.herokuapp.com/',
+  server: '127.0.0.1',
   database: 'Blog',
   options: {
     trustedConnection: true,
