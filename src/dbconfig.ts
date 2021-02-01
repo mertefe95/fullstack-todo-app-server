899779261476c7d96f857c16bab376f2a3b20107
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-01 12:26:22 +0300

    cng

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -1,12 +1,11 @@
 const config = {
   user: 'MertAdmin',
   password: 'Admin12345',
-  server: '127.0.0.1',
+  server: 'MERT\\MSSQLSERVER01',
   database: 'Blog',
   options: {
     trustedConnection: true,
     enableArithAort: true,
-    instanceName: `MSSQLSERVER01`,
     parseJSON:true
   },
   pool: {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import fastify from 'fastify';
 import { UserRoute } from "./routes/UserRoute";
 import { PostRoute } from "./routes/PostRoute";
+import fastify-cors from "fastify-cors";
+
 
 const server = fastify();
 
 
 server.register(UserRoute);
 server.register(PostRoute);
+server.register(fastify-cors, {
+  
+})
 
 const PORT = process.env.PORT || 8080;
 
@@ -16,4 +21,4 @@ server.listen(PORT, (err: any, address:any) => {
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
-})
+})  
