8829476fe48ec815f4fbd4ba490d2e213038d505
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-01 11:24:53 +0300

    chng

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -8,7 +8,12 @@ const config = {
     enableArithAort: true,
     instanceName: `MSSQLSERVER01`,
     parseJSON:true
-  }
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+}
 }
 
 export default config
