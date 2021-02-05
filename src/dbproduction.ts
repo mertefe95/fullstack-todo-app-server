1c80efe5e59c87dc0b917560ffdb70529918b95d
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-05 16:17:04 +0300

    qewqwe,

--- src/dbproduction.ts
+++ src/dbproduction.ts
@@ -1,9 +1,11 @@
 const { Pool } = require("pg");
-const connectionString = process.env.DATABASE_URL
 
 const pool = new Pool({
-  connectionString,
-  ssl: true
+  user: "postgres",
+  password: "7668498760361d2363e2e21d7bead6c5e3b5d378963bbc0aa74f1cb032a6c1b7",
+  host: "ec2-52-6-178-202.compute-1.amazonaws.com",
+  port: 5432,
+  database: "dceniu2kram4t3",
 });
 
 export default pool;
