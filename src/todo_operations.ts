3397e3bc64e1679ec547d3f676374c080c883668
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-05 15:58:12 +0300

    chng

--- src/todo_operations.ts
+++ src/todo_operations.ts
@@ -1,8 +1,14 @@
 import pool from "./db";
+import client from "./dbproduction";
+
+
+client.connect();
+
+
 
 async function getAllTodos() {
   try {
-    const posts = await pool.query("SELECT * from todos");
+    const posts = await client.query("SELECT * from todos");
     return posts.rows;
   } catch (error) {
     console.log(error);
@@ -11,7 +17,7 @@ async function getAllTodos() {
 
 async function getTodo(id: any) {
   try {
-    const todo = await pool.query(`SELECT * from todos where id = ${id}`);
+    const todo = await client.query(`SELECT * from todos where id = ${id}`);
     return todo.rows[0];
   } catch (error) {
     console.log(error);
@@ -20,7 +26,7 @@ async function getTodo(id: any) {
 
 async function addTodo(newTodo: any) {
   try {
-    const insertTodo = await pool.query(`INSERT INTO todos (title, text, author, userId) 
+    const insertTodo = await client.query(`INSERT INTO todos (title, text, author, userId) 
             values 
             ('${newTodo.title}', '${newTodo.text}', '${newTodo.author}', ${newTodo.userId})`);
     return insertTodo.rows[0];
@@ -31,7 +37,7 @@ async function addTodo(newTodo: any) {
 
 async function deleteTodo(id: number) {
   try {
-    const deletedTodo = await pool.query(`DELETE FROM todos WHERE id = ${id}`);
+    const deletedTodo = await client.query(`DELETE FROM todos WHERE id = ${id}`);
     return deletedTodo.rows[0];
   } catch (err) {
     console.log(err);
@@ -40,7 +46,7 @@ async function deleteTodo(id: number) {
 
 async function updateTodo(id: number, title: string, text: string) {
   try {
-    const updateTodo = await pool.query(
+    const updateTodo = await client.query(
       `UPDATE todos SET title = '${title}', text = '${text}' WHERE id = ${id}`
     );
     return updateTodo.rows[0];
@@ -48,5 +54,6 @@ async function updateTodo(id: number, title: string, text: string) {
     console.log(err);
   }
 }
+client.end();
 
 export { getAllTodos, getTodo, addTodo, deleteTodo, updateTodo };
